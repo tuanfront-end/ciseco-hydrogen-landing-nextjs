9b8da6941f91ed829d3fd021963cddac783f0317
Chis Nghiax <atuanvptd@gmail.com>
2024-05-14 22:21:27 +0700

    fix chinhta

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -18,10 +18,10 @@ export function Introduction() {
           the features of an online store.
         </p>
         <p className="mt-4">
-          This template is suited for multi vendor marketplace, electronics
-          store, furnitures store, clothings store, hi-tech store, organic/food
-          store, cosmetic store, jewelry store, sporting goods store and
-          accessories store, etc.
+          This template is suited for multi-vendor marketplace, electronics
+          stores, furniture stores, clothing stores, hi-tech stores,
+          organic/food stores, cosmetic stores, jewelry stores, sporting goods
+          stores, and accessories stores, etc.
         </p>
 
         <p className="mt-4">Some “key highlights” of the Ciseco:</p>
@@ -46,7 +46,7 @@ export function Introduction() {
         <p className="mt-8">
           The Ciseco template is built with the latest technologies such as
           React v18.x, Tailwindcss v3.x, Typescript, and HeadlessUI components.
-          It is also built with the latest version of Shopify’s Hydrogen 2024 ,
+          It is also built with the latest version of Shopify’s Hydrogen 2024,
           which provides users with a fast and reliable storefront.
         </p>
         <p className="mt-10">
