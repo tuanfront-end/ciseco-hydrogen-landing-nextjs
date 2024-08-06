75f45ad148f1235db9903d7dc3ecb683a54b6fc5
Chis Nghiax <atuanvptd@gmail.com>
2024-08-06 09:51:33 +0700

    updsate preview link to cisec0.h2.nghiaxchis

--- src/data/data.ts
+++ src/data/data.ts
@@ -3,8 +3,7 @@ export const DATA = {
     'https://nghiaxchis.gitbook.io/ciseco-hydrogen-shopifys-headless-theme',
   changeLogLink:
     'https://nghiaxchis.gitbook.io/ciseco-hydrogen-shopifys-headless-theme/change-log/change-log',
-  previewLink:
-    'https://ciseco-hydrogen-vite-56385ad898867bbff28c.o2.myshopify.dev',
+  previewLink: 'https://ciseco-h2.chisnghiax.com',
   buyLink:
     'https://themeforest.net/item/ciseco-hydrogen-shopifys-headless-storefront-template/52088296',
   price: '$49',
